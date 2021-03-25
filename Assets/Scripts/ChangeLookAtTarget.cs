795eb8b737209d750ac9c6236706b0b8c1260d0a
AzaliaPe <azaliaphdz@gmail.com>
2021-03-24 21:05:49 -0700

    DistintasVistas

---
+++ Assets/Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLookAtTarget : MonoBehaviour 
+{
+
+	public GameObject target; 
+
+	void Start() 
+	{
+		if (target == null) 
+		{
+			target = this.gameObject;
+		}
+	}
+
+	void OnMouseDown () 
+	{
+		LookAtTarget.target = target;
+        Camera.main.fieldOfView = Mathf.Clamp(60 * target.transform.localScale.x, 1, 100);
+	}
+}
